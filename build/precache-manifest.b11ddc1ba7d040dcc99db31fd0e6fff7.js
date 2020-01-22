ac28fa71b2e728fd4bed836e075f33522b9bb8a2
DigiCurious <cinematography@oskaripolho.com>
2020-01-22 12:51:49 +0000

    Add files via upload

---
+++ build/precache-manifest.b11ddc1ba7d040dcc99db31fd0e6fff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d43681aabd893c6467b805e34e3248c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddf1b32be713e97d1862",
+    "url": "/static/css/main.d39de011.chunk.css"
+  },
+  {
+    "revision": "c92111e4c2436d2e9058",
+    "url": "/static/js/2.585a3759.chunk.js"
+  },
+  {
+    "revision": "8de4bdbb127f280cc6b79279dd69f244",
+    "url": "/static/js/2.585a3759.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ddf1b32be713e97d1862",
+    "url": "/static/js/main.1f4c54d0.chunk.js"
+  },
+  {
+    "revision": "b5c1efa8f474acf368e3",
+    "url": "/static/js/runtime-main.07fb1af6.js"
+  }
+]);
