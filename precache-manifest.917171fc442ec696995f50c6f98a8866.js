9f45f277e52ba77ee721f45c9e5340c83553c32b
Oskari Polho <oskaripolho@Oskaris-MacBook-Pro.local>
2020-01-22 09:00:27 +0000

    Updates

---
+++ precache-manifest.917171fc442ec696995f50c6f98a8866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5181cb64cebfea7429394788c2f4fe51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12f11e4cda53f309402a",
+    "url": "/static/css/main.d39de011.chunk.css"
+  },
+  {
+    "revision": "c92111e4c2436d2e9058",
+    "url": "/static/js/2.585a3759.chunk.js"
+  },
+  {
+    "revision": "8de4bdbb127f280cc6b79279dd69f244",
+    "url": "/static/js/2.585a3759.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12f11e4cda53f309402a",
+    "url": "/static/js/main.ea07060e.chunk.js"
+  },
+  {
+    "revision": "b5c1efa8f474acf368e3",
+    "url": "/static/js/runtime-main.07fb1af6.js"
+  }
+]);
